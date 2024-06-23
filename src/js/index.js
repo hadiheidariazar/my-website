9511c033da04a21794567ac6903e7dd814ef7614
hadiheidariazar <hadiha010101@gmail.com>
2024-06-23 23:40:29 +0330

    header section feature coding in the index.js file

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,33 @@
 const $ = document
 
 const loader = $.querySelector('.loader')
+const menuIcon = $.querySelector('.toggle-menu')
+const phoneDeviceMenu = $.querySelector('.phone-menu')
+const navbarItemLink = $.querySelectorAll('.scroll-section')
 
 window.addEventListener('load', () => {
     $.body.classList.remove('overflow-hidden')
     loader.classList.remove('flex')
     loader.classList.add('hidden')
+})
+
+menuIcon.addEventListener('click', () => {
+    phoneDeviceMenu.classList.toggle('hidden')
+
+    $.addEventListener('click', (event) => {
+        if (phoneDeviceMenu.classList[2] !== 'hidden') {
+            if (event.target.classList[event.target.classList.length - 1] !== 'header-navbar') {
+                phoneDeviceMenu.classList.add('hidden')
+            }
+        }
+    })
+})
+
+function scrollToMainSection(targetElement) {
+    const mainSectionLocation = $.querySelector(`section#${targetElement}`).offsetTop
+    window.scrollTo({ top: (mainSectionLocation - 120), behavior: 'smooth' })
+}
+
+navbarItemLink.forEach(btn => {
+    btn.addEventListener('click', () => scrollToMainSection(btn.getAttribute('id')))
 })
